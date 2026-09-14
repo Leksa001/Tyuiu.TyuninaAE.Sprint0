be7a0e4fd7e163aaa7b180a03df0bc071ce5999a
Leksa001 <206306046+Leksa001@users.noreply.github.com>
2026-09-14 23:06:12 +0500

    Task1

---
+++ Tyuiu.TyuninaAE.Sprint0.Task1.V0/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.TyuninaAE.Sprint0.Task1.V0")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.TyuninaAE.Sprint0.Task1.V0")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c4cf7746-8db2-416b-a8d4-a1507f9f1fd0")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
