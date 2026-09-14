cc5715dcc719b98aad02a488ba45206efb94b531
Leksa001 <206306046+Leksa001@users.noreply.github.com>
2026-09-14 23:27:54 +0500

    Task4

---
+++ Tyuiu.TyuninaAE.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.TyuninaAE.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyProduct("Tyuiu.TyuninaAE.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a1b1e0e8-50aa-4944-bc22-97ed52483342")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
